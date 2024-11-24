387604ca58f4d1ac93f05bbacb54d7b9baf2e55b
Shadow <julianlee555@outlook.com>
2024-11-24 10:05:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -27,12 +27,36 @@ function displayMessage(message, className) {
 }
 
 function getBotResponse(input) {
-  // Example logic
-  if (input.toLowerCase().includes("mod")) {
-    return "Check out this mod: [Mod Download Link]";
-  } else if (input.toLowerCase().includes("resource pack")) {
-    return "Here's a resource pack: [Resource Pack Link]";
+  input = input.toLowerCase(); // Normalize input for case insensitivity
+
+  // Match user input with available content
+  if (input.includes("magic way")) {
+    return "Download 'A Magic Way': [content/AMagicWay.zip]";
+  } else if (input.includes("guns") || input.includes("actual guns")) {
+    return "Download 'Actual Guns 3D': [content/ActualGuns_3D_1.8.1.mcaddon]";
+  } else if (input.includes("baby ender dragon")) {
+    return "Download 'Baby Ender Dragon': [content/BabyEnderDragon.mcaddon]";
+  } else if (input.includes("elemental wands")) {
+    return "Download 'Elemental Wands': [content/ElementalWands.zip]";
+  } else if (input.includes("ender awakening")) {
+    return "Download 'Ender Awakening': [content/EnderAwakening.zip]";
+  } else if (input.includes("mob vote losers")) {
+    return "Download 'Mob Vote Losers': [content/MobVoteLosers.mcaddon]";
+  } else if (input.includes("shadows classic") || input.includes("slim skins")) {
+    return "Download 'Shadows Classic and Slim Skins': [content/ShadowsClasicAndSlimCompilationSkins.zip]";
+  } else if (input.includes("shadows resources")) {
+    return "Download 'Shadows Resources': [content/ShadowsResources.zip]";
+  } else if (input.includes("shadows skins")) {
+    return "Download 'Shadows Skins': [content/ShadowsSkins.zip]";
+  } else if (input.includes("spry conquest")) {
+    return "Download 'Spry Conquest': [content/SpryConquest.mcaddon]";
+  } else if (input.includes("tnt gun")) {
+    return "Download 'TNT Gun': [content/TntGun.mcaddon]";
+  } else if (input.includes("true weapons")) {
+    return "Download 'True Weapons': [content/TrueWeapons.zip]";
+  } else if (input.includes("camouflage skin")) {
+    return "Download 'Camouflage Skin Pack': [content/camouflage-skin-pack.mcpack]";
   } else {
-    return "Sorry, I don't understand that. Try asking about mods or resource packs!";
+    return "Sorry, I couldn't find anything related to your query. Try asking about specific mods or resource packs!";
   }
 }
