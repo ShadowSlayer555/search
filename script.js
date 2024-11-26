22f9be8ff3625a217679a9a241c716e91cf4127b
Shadow <julianlee555@outlook.com>
2024-11-25 18:28:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -31,31 +31,31 @@ function getBotResponse(input) {
 
   // Match user input with available content
   if (input.includes("magic way")) {
-    return "Download 'A Magic Way': [content/AMagicWay.zip]";
+    return "Download 'A Magic Way': <a href='content/AMagicWay.zip' target='_blank'>Click here</a>";
   } else if (input.includes("guns") || input.includes("actual guns")) {
-    return "Download 'Actual Guns 3D': [content/ActualGuns_3D_1.8.1.mcaddon]";
+    return "Download 'Actual Guns 3D': <a href='content/ActualGuns_3D_1.8.1.mcaddon' target='_blank'>Click here</a>";
   } else if (input.includes("baby ender dragon")) {
-    return "Download 'Baby Ender Dragon': [content/BabyEnderDragon.mcaddon]";
+    return "Download 'Baby Ender Dragon': <a href='content/BabyEnderDragon.mcaddon' target='_blank'>Click here</a>";
   } else if (input.includes("elemental wands")) {
-    return "Download 'Elemental Wands': [content/ElementalWands.zip]";
+    return "Download 'Elemental Wands': <a href='content/ElementalWands.zip' target='_blank'>Click here</a>";
   } else if (input.includes("ender awakening")) {
-    return "Download 'Ender Awakening': [content/EnderAwakening.zip]";
+    return "Download 'Ender Awakening': <a href='content/EnderAwakening.zip' target='_blank'>Click here</a>";
   } else if (input.includes("mob vote losers")) {
-    return "Download 'Mob Vote Losers': [content/MobVoteLosers.mcaddon]";
+    return "Download 'Mob Vote Losers': <a href='content/MobVoteLosers.mcaddon' target='_blank'>Click here</a>";
   } else if (input.includes("shadows classic") || input.includes("slim skins")) {
-    return "Download 'Shadows Classic and Slim Skins': [content/ShadowsClasicAndSlimCompilationSkins.zip]";
+    return "Download 'Shadows Classic and Slim Skins': <a href='content/ShadowsClasicAndSlimCompilationSkins.zip' target='_blank'>Click here</a>";
   } else if (input.includes("shadows resources")) {
-    return "Download 'Shadows Resources': [content/ShadowsResources.zip]";
+    return "Download 'Shadows Resources': <a href='content/ShadowsResources.zip' target='_blank'>Click here</a>";
   } else if (input.includes("shadows skins")) {
-    return "Download 'Shadows Skins': [content/ShadowsSkins.zip]";
+    return "Download 'Shadows Skins': <a href='content/ShadowsSkins.zip' target='_blank'>Click here</a>";
   } else if (input.includes("spry conquest")) {
-    return "Download 'Spry Conquest': [content/SpryConquest.mcaddon]";
+    return "Download 'Spry Conquest': <a href='content/SpryConquest.mcaddon' target='_blank'>Click here</a>";
   } else if (input.includes("tnt gun")) {
-    return "Download 'TNT Gun': [content/TntGun.mcaddon]";
+    return "Download 'TNT Gun': <a href='content/TntGun.mcaddon' target='_blank'>Click here</a>";
   } else if (input.includes("true weapons")) {
-    return "Download 'True Weapons': [content/TrueWeapons.zip]";
+    return "Download 'True Weapons': <a href='content/TrueWeapons.zip' target='_blank'>Click here</a>";
   } else if (input.includes("camouflage skin")) {
-    return "Download 'Camouflage Skin Pack': [content/camouflage-skin-pack.mcpack]";
+    return "Download 'Camouflage Skin Pack': <a href='content/camouflage-skin-pack.mcpack' target='_blank'>Click here</a>";
   } else {
     return "Sorry, I couldn't find anything related to your query. Try asking about specific mods or resource packs!";
   }
