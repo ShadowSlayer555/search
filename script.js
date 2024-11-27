f1280a039a149280f275bc8137f085b9fec7bc35
Shadow <julianlee555@outlook.com>
2024-11-26 18:01:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -37,26 +37,23 @@ function displayMessage(message, className) {
 function levenshteinDistance(a, b) {
   const matrix = [];
 
-  // Increment along the first column of each row
   for (let i = 0; i <= b.length; i++) {
     matrix[i] = [i];
   }
 
-  // Increment along the first row of each column
   for (let j = 0; j <= a.length; j++) {
     matrix[0][j] = j;
   }
 
-  // Fill in the rest of the matrix
   for (let i = 1; i <= b.length; i++) {
     for (let j = 1; j <= a.length; j++) {
       if (b.charAt(i - 1) === a.charAt(j - 1)) {
         matrix[i][j] = matrix[i - 1][j - 1];
       } else {
         matrix[i][j] = Math.min(
-          matrix[i - 1][j - 1] + 1, // Substitution
-          matrix[i][j - 1] + 1,     // Insertion
-          matrix[i - 1][j] + 1      // Deletion
+          matrix[i - 1][j - 1] + 1,
+          matrix[i][j - 1] + 1,
+          matrix[i - 1][j] + 1
         );
       }
     }
@@ -67,19 +64,32 @@ function levenshteinDistance(a, b) {
 
 // Content map for matching
 const contentMap = {
-  "magic way": "Download 'A Magic Way': <a href='content/AMagicWay.zip' target='_blank'>Click here</a>",
-  "guns": "Download 'Actual Guns 3D': <a href='content/ActualGuns_3D_1.8.1.mcaddon' target='_blank'>Click here</a>",
-  "baby ender dragon": "Download 'Baby Ender Dragon': <a href='content/BabyEnderDragon.mcaddon' target='_blank'>Click here</a>",
-  "elemental wands": "Download 'Elemental Wands': <a href='content/ElementalWands.zip' target='_blank'>Click here</a>",
-  "ender awakening": "Download 'Ender Awakening': <a href='content/EnderAwakening.zip' target='_blank'>Click here</a>",
-  "mob vote losers": "Download 'Mob Vote Losers': <a href='content/MobVoteLosers.mcaddon' target='_blank'>Click here</a>",
-  "skins": "Download 'Shadows Classic and Slim Skins': <a href='content/ShadowsClasicAndSlimCompilationSkins.zip' target='_blank'>Click here</a> 'Shadows Skins': <a href='content/ShadowsSkins.zip' target='_blank'>Click here</a>",
-  "shadows resources": "Download 'Shadows Resources': <a href='content/ShadowsResources.zip' target='_blank'>Click here</a>",
-  "spry conquest": "Download 'Spry Conquest': <a href='content/SpryConquest.mcaddon' target='_blank'>Click here</a>",
-  "tnt gun": "Download 'TNT Gun': <a href='content/TntGun.mcaddon' target='_blank'>Click here</a>",
-  "true weapons": "Download 'True Weapons': <a href='content/TrueWeapons.zip' target='_blank'>Click here</a>",
-  "camo": "Download 'Camouflage Skin Pack': <a href='content/camouflage-skin-pack.mcpack' target='_blank'>Click here</a>",
-  "mods": "Here are some options: 'A Magic Way': <a href='content/AMagicWay.zip' target='_blank'>Click here</a> 'Actual Guns 3D': <a href='content/ActualGuns_3D_1.8.1.mcaddon' target='_blank'>Click here</a> Download 'Baby Ender Dragon': <a href='content/BabyEnderDragon.mcaddon' target='_blank'>Click here</a> Download 'Elemental Wands': <a href='content/ElementalWands.zip' target='_blank'>Click here</a> Download 'Mob Vote Losers': <a href='content/MobVoteLosers.mcaddon' target='_blank'>Click here</a> 'Spry Conquest': <a href='content/SpryConquest.mcaddon' target='_blank'>Click here</a> 'TNT Gun': <a href='content/TntGun.mcaddon' target='_blank'>Click here</a> 'True Weapons': <a href='content/TrueWeapons.zip' target='_blank'>Click here</a>,
+  "magic way": "<a href='content/AMagicWay.zip' target='_blank'>Download 'A Magic Way'</a>",
+  "guns": "<a href='content/ActualGuns_3D_1.8.1.mcaddon' target='_blank'>Download 'Actual Guns 3D'</a>",
+  "baby ender dragon": "<a href='content/BabyEnderDragon.mcaddon' target='_blank'>Download 'Baby Ender Dragon'</a>",
+  "elemental wands": "<a href='content/ElementalWands.zip' target='_blank'>Download 'Elemental Wands'</a>",
+  "ender awakening": "<a href='content/EnderAwakening.zip' target='_blank'>Download 'Ender Awakening'</a>",
+  "mob vote losers": "<a href='content/MobVoteLosers.mcaddon' target='_blank'>Download 'Mob Vote Losers'</a>",
+  "shadows classic skins": "<a href='content/ShadowsClasicAndSlimCompilationSkins.zip' target='_blank'>Download 'Shadows Classic and Slim Skins'</a>",
+  "shadows resources": "<a href='content/ShadowsResources.zip' target='_blank'>Download 'Shadows Resources'</a>",
+  "shadows skins": "<a href='content/ShadowsSkins.zip' target='_blank'>Download 'Shadows Skins'</a>",
+  "spry conquest": "<a href='content/SpryConquest.mcaddon' target='_blank'>Download 'Spry Conquest'</a>",
+  "tnt gun": "<a href='content/TntGun.mcaddon' target='_blank'>Download 'TNT Gun'</a>",
+  "true weapons": "<a href='content/TrueWeapons.zip' target='_blank'>Download 'True Weapons'</a>",
+  "camouflage skin": "<a href='content/camouflage-skin-pack.mcpack' target='_blank'>Download 'Camouflage Skin Pack'</a>",
+  "mods": `
+    <p>Here are some mods you can try:</p>
+    <ul>
+      <li><a href='content/AMagicWay.zip' target='_blank'>A Magic Way</a></li>
+      <li><a href='content/ActualGuns_3D_1.8.1.mcaddon' target='_blank'>Actual Guns 3D</a></li>
+      <li><a href='content/BabyEnderDragon.mcaddon' target='_blank'>Baby Ender Dragon</a></li>
+      <li><a href='content/ElementalWands.zip' target='_blank'>Elemental Wands</a></li>
+      <li><a href='content/MobVoteLosers.mcaddon' target='_blank'>Mob Vote Losers</a></li>
+      <li><a href='content/SpryConquest.mcaddon' target='_blank'>Spry Conquest</a></li>
+      <li><a href='content/TntGun.mcaddon' target='_blank'>TNT Gun</a></li>
+      <li><a href='content/TrueWeapons.zip' target='_blank'>True Weapons</a></li>
+    </ul>
+  `
 };
 
 function getBotResponse(input) {
